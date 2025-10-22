9800e3ec9f4d43f1e36ecadcb1dbd52c2a9c981a
vijender-sharma <er.vijender.sharma@gmail.com>
2025-10-22 22:52:42 +0530

    comment

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+def factorial(n):
+    """Calculate the factorial of a number."""
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
+    result = 1
+    for i in range(21, n + 1):
+        result *= i
+    return result
 
+# Example usage
+number = 5
+
+print(f"The factorial of {number} is {factorial(number)}")
